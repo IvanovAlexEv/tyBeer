32906c69e49c0c502531aced1eff92127c439e52
Aleksandar Evgeniev Ivanov <ivanov.aleksandar.ev@gmail.com>
2021-03-28 11:19:27 +0200

    Starting. v1.0

--- app/src/main/java/Package/tyBeer/MainActivity.kt
+++ app/src/main/java/Package/tyBeer/MainActivity.kt
@@ -1,11 +1,28 @@
 package Package.tyBeer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.Animator
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        LottieLogo.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator?) {}
+            override fun onAnimationEnd(animation: Animator?) {
+                openHome()
+            }
+            override fun onAnimationCancel(animation: Animator?) {}
+            override fun onAnimationRepeat(animation: Animator?) {}
+        })
+    }
+    fun openHome(){
+        val intentHome = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(intentHome)
+        finish()
     }
 }
+
