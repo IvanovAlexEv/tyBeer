e86eb268b091d24b9471f03cf7cb035e7ca5d0e2
Aleksandar Evgeniev Ivanov <ivanov.aleksandar.ev@gmail.com>
2021-03-29 19:12:56 +0200

    Login. v1.0

--- app/src/main/java/Package/tyBeer/LoginActivity.kt
+++ app/src/main/java/Package/tyBeer/LoginActivity.kt
@@ -1,11 +1,34 @@
 package Package.tyBeer
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+    fun checkLogin(v: View?){
+        val email:String = editText_emailLogin.text.toString()
+        val pax:String = editText_paxLogin.text.toString()
+        if (!isValidEmail(email)){
+            editText_emailLogin.error = getString(R.string.str_errorEmail)
+        }
+        if (!isValidPassword(pax)){
+            editText_paxLogin.error = getString(R.string.str_errorPax)
+        }
+    }
+    private fun isValidEmail(email:String):Boolean{
+        val EMAIL_PATTERN = ("^[_A-Za-z0-9-\\+]+(\\.[_+A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")
+        val pattern = Pattern.compile(EMAIL_PATTERN)
+        val matcher = pattern.matcher(email)
+        return matcher.matches()
+    }
+    private fun isValidPassword(pax:String?):Boolean {
+        return (pax != null && pax.length>=4)
     }
 }
--- app/src/main/java/Package/tyBeer/MainActivity.kt
+++ app/src/main/java/Package/tyBeer/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        LottieLogo.addAnimatorListener(object : Animator.AnimatorListener {
+        LottieBeer.addAnimatorListener(object : Animator.AnimatorListener {
             override fun onAnimationStart(animation: Animator?) {}
             override fun onAnimationEnd(animation: Animator?) {
                 openHome()
