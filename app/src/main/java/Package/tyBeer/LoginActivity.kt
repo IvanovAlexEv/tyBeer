abae5b6fb66fb050ba7cbca8121fe7d3d631e38f
Aleksandar Evgeniev Ivanov <ivanov.aleksandar.ev@gmail.com>
2021-03-31 18:16:42 +0200

    Updates. v1.2

--- app/src/main/java/Package/tyBeer/LoginActivity.kt
+++ app/src/main/java/Package/tyBeer/LoginActivity.kt
@@ -3,6 +3,7 @@ package Package.tyBeer
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import java.util.regex.Pattern
@@ -15,11 +16,27 @@ class LoginActivity : AppCompatActivity() {
     fun checkLogin(v: View?){
         val email:String = editText_emailLogin.text.toString()
         val pax:String = editText_paxLogin.text.toString()
+        var emailOkay:Boolean = false
         if (!isValidEmail(email)){
             editText_emailLogin.error = getString(R.string.str_errorEmail)
+        } else {
+            if (!isEmailFound(email)){
+                editText_emailLogin.error = getString(R.string.str_errorEmailNotFound)
+            } else {
+                emailOkay = true
+            }
         }
         if (!isValidPassword(pax)){
             editText_paxLogin.error = getString(R.string.str_errorPax)
+        } else {
+            if (emailOkay){
+                if (!isPasswordMatch(pax,email)){
+                    editText_paxLogin.error = getString(R.string.str_errorPaxNotMatch)
+                } else {
+                    // LOGIN...
+                    Toast.makeText(this@LoginActivity, "LOGIN...", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
     private fun isValidEmail(email:String):Boolean{
@@ -29,8 +46,28 @@ class LoginActivity : AppCompatActivity() {
         val matcher = pattern.matcher(email)
         return matcher.matches()
     }
+    private fun isEmailFound(email:String):Boolean{
+        // CONTROLLO ESISTENZA EMAIL
+        return false
+    }
     private fun isValidPassword(pax:String?):Boolean {
-        return (pax != null && pax.length>=8)
+        var UpperCase: Boolean = false
+        var LowerCase: Boolean = false
+        var Number: Boolean = false
+        var Length: Boolean = false
+        if (pax != null) {
+            for (i in pax) {
+                if (i.isUpperCase()) UpperCase = true
+                if (i.isLowerCase()) LowerCase = true
+                if (i.isDigit()) Number = true
+            }
+            if (pax.length >= 8) Length = true
+        }
+        return (UpperCase && LowerCase && Number && Length)
+    }
+    private fun isPasswordMatch(pax:String?, email:String?):Boolean {
+        // CONTROLLO ESISTENZA PASSWORD
+        return false
     }
 
     fun openRegister(v:View){
--- app/src/main/java/Package/tyBeer/SigninActivity.kt
+++ app/src/main/java/Package/tyBeer/SigninActivity.kt
@@ -3,6 +3,9 @@ package Package.tyBeer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_signin.*
+import java.util.regex.Pattern
 
 class SigninActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +13,97 @@ class SigninActivity : AppCompatActivity() {
         setContentView(R.layout.activity_signin)
     }
 
+    fun checkRegister(v: View?){
+        val username:String = editText_UsernameRegister.text.toString()
+        val email:String = editText_emailRegister.text.toString()
+        val pax:String = editText_paxRegister.text.toString()
+        val paxRepeat:String = editText_paxRegisterRepeat.text.toString()
+        val name:String = editText_NameRegister.text.toString()
+        val surname:String = editText_NameRegister.text.toString()
+        var isAllOkay:Boolean = true
+
+        // CONTROLLO DATI
+        if (!isValidUsername(username)){
+            editText_UsernameRegister.error =getString(R.string.str_errorUsername)
+            isAllOkay=false
+        }
+        if (!isUsernameFree(username)){
+            editText_UsernameRegister.error =getString(R.string.str_errorUsernameNotFree)
+            isAllOkay=false
+        }
+        if (!isValidNameOrSurname(name)){
+            editText_NameRegister.error =getString(R.string.str_errorNameOrSurname)
+            isAllOkay=false
+        }
+        if (!isValidNameOrSurname(surname)){
+            editText_SurnameRegister.error =getString(R.string.str_errorNameOrSurname)
+            isAllOkay=false
+        }
+        if (!isValidEmail(email)){
+            editText_emailRegister.error = getString(R.string.str_errorEmail)
+            isAllOkay=false
+        }
+        if (!isEmailFree(email)){
+            editText_emailRegister.error = getString(R.string.str_errorEmailNotFree)
+            isAllOkay=false
+        }
+        if (!isValidPassword(pax)){
+            editText_paxRegister.error = getString(R.string.str_errorPax)
+            isAllOkay=false
+        }
+        if (!pax.equals(paxRepeat)){
+            editText_paxRegisterRepeat.error = getString(R.string.str_errorPaxEquals)
+            isAllOkay=false
+        }
+        if (isAllOkay) {
+            // REGISTRAZIONE...
+            Toast.makeText(this@SigninActivity, "REGISTRAZIONE...", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun isValidUsername(user:String?):Boolean {
+        var valid:Boolean=true
+        if (user != null) {
+            if (user.length<4) valid=false
+            for (i in user) {
+                if (i.equals(" ")) valid=false
+                if (!i.isLetterOrDigit()) valid=false
+            }
+        } else return false
+        return valid
+    }
+    private fun isUsernameFree(user:String?):Boolean {
+        //  CONTROLLO DISPONIBILITA' USERNAME
+        return true
+    }
+    private fun isValidNameOrSurname(NorS:String?):Boolean{
+        return !(NorS.equals(""))
+    }
+    private fun isValidEmail(email:String):Boolean{
+        val EMAIL_PATTERN = ("^[_A-Za-z0-9-\\+]+(\\.[_+A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")
+        val pattern = Pattern.compile(EMAIL_PATTERN)
+        val matcher = pattern.matcher(email)
+        return matcher.matches()
+    }
+    private fun isEmailFree(email:String):Boolean{
+        //  CONTROLLO DISPONIBILITA' EMAIL
+        return true
+    }
+    private fun isValidPassword(pax:String?):Boolean {
+        var UpperCase:Boolean=false
+        var LowerCase:Boolean=false
+        var Number:Boolean=false
+        var Length:Boolean=false
+        if (pax != null) {
+            for (i in pax){
+                if (i.isUpperCase()) UpperCase=true
+                if (i.isLowerCase()) LowerCase=true
+                if (i.isDigit()) Number=true
+            }
+            if (pax.length>=8)  Length=true
+        }
+        return ( UpperCase && LowerCase && Number && Length)
+    }
     fun openLogin(v: View){
         finish()
     }
