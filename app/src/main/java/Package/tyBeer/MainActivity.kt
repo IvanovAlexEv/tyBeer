6a3a3553e9cae7d537992fab8fc526c98b510a0d
Aleksandar Evgeniev Ivanov <ivanov.aleksandar.ev@gmail.com>
2021-04-22 17:07:11 +0200

    Updates. v1.2.1

--- app/src/main/java/Package/tyBeer/MainActivity.kt
+++ app/src/main/java/Package/tyBeer/MainActivity.kt
@@ -2,6 +2,7 @@ package Package.tyBeer
 
 import android.animation.Animator
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,6 +19,8 @@ class MainActivity : AppCompatActivity() {
             override fun onAnimationCancel(animation: Animator?) {}
             override fun onAnimationRepeat(animation: Animator?) {}
         })
+        val SoundBeer: MediaPlayer = MediaPlayer.create(this,R.raw.sound_beer)
+        SoundBeer.start()
     }
     fun openHome(){
         val intentHome = Intent(this@MainActivity, LoginActivity::class.java)
